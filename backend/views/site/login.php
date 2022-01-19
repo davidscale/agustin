0a81ff9356867a9e4ca24d960c9bfc3808ca2487
matirue <matiasrue@gmail.com>
2022-01-19 03:06:47 +0000

    probando..

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -7,7 +7,7 @@
 use yii\bootstrap4\ActiveForm;
 use yii\bootstrap4\Html;
 
-$this->title = 'Login';
+$this->title = 'Iniciar Sesión';
 ?>
 <div class="site-login">
     <div class="mt-5 offset-lg-3 col-lg-6">
